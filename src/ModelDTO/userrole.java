7445cca8678a9f11a67a3d0d3448dc8106ad6c43
kepisal <kepisal@gmail.com>
2015-07-30 11:26:46 +0700

    DTO Class
    6 of classes created

---
+++ src/ModelDTO/userrole.java
@@ -0,0 +1,35 @@
+package ModelDTO;
+
+/**
+ * @author Pisal
+ *
+ */
+public class userrole {
+	private int user_role_id;
+	private String user_type;
+	private String user_desc;
+
+	public int getUser_role_id() {
+		return user_role_id;
+	}
+
+	public void setUser_role_id(int user_role_id) {
+		this.user_role_id = user_role_id;
+	}
+
+	public String getUser_type() {
+		return user_type;
+	}
+
+	public void setUser_type(String user_type) {
+		this.user_type = user_type;
+	}
+
+	public String getUser_desc() {
+		return user_desc;
+	}
+
+	public void setUser_desc(String user_desc) {
+		this.user_desc = user_desc;
+	}
+}
