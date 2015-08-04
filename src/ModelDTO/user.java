d1a6d128afa4d622fdd8b454e5bb534b8137a01c
kepisal <kepisal@gmail.com>
2015-08-04 10:21:44 +0700

    change to Uppercase
    change to Uppercase

--- src/ModelDTO/user.java
+++ src/ModelDTO/user.java
@@ -10,49 +10,49 @@ import java.io.Serializable;
  *
  */
 public class User implements Serializable {
-	private int user_id;
-	private String user_name;
-	private String user_pass;
-	private String user_type;
+	private int User_id;
+	private String User_name;
+	private String User_pass;
+	private String User_type;
 public User(){}
-	public User(int user_id, String user_name, String user_pass,
-			String user_type) {
+	public User(int User_id, String User_name, String User_pass,
+			String User_type) {
 		super();
-		this.user_id = user_id;
-		this.user_name = user_name;
-		this.user_pass = user_pass;
-		this.user_type = user_type;
+		this.User_id = User_id;
+		this.User_name = User_name;
+		this.User_pass = User_pass;
+		this.User_type = User_type;
 	}
 
 	public int getUser_id() {
-		return user_id;
+		return User_id;
 	}
 
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
+	public void setUser_id(int User_id) {
+		this.User_id = User_id;
 	}
 
 	public String getUser_name() {
-		return user_name;
+		return User_name;
 	}
 
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
+	public void setUser_name(String User_name) {
+		this.User_name = User_name;
 	}
 
 	public String getUser_pass() {
-		return user_pass;
+		return User_pass;
 	}
 
-	public void setUser_pass(String user_pass) {
-		this.user_pass = user_pass;
+	public void setUser_pass(String User_pass) {
+		this.User_pass = User_pass;
 	}
 
 	public String getUser_type() {
-		return user_type;
+		return User_type;
 	}
 
-	public void setUser_type(String user_type) {
-		this.user_type = user_type;
+	public void setUser_type(String User_type) {
+		this.User_type = User_type;
 	}
 }
