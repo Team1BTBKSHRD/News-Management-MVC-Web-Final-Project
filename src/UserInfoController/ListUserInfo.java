9355813d79d4d4583c2981d46984642291d9b7ad
kepisal <kepisal@gmail.com>
2015-08-06 17:07:57 +0700

    Add more UserInfo Controll
    Add more UserInfo Controll

---
+++ src/UserInfoController/ListUserInfo.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package UserInfoController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import Controller.Action;
+import Controller.ActionForward;
+import ModelDAO.UserDAO;
+import Utilities.Convertor;
+
+/**
+ * @author Pisal
+ *
+ */
+public class ListUserInfo implements Action {
+
+	/* (non-Javadoc)
+	 * @see Controller.Action#execute(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		String obj= new Gson().toJson(new Convertor().convertResultSetIntoJSON(new UserDAO().retrieveRS()));
+		
+		response.getWriter().write(obj);		
+		return null;
+	}
+
+}
---
+++ src/UserInfoController/RemoveUserInfo.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package UserInfoController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Controller.Action;
+import Controller.ActionForward;
+import ModelDAO.UserDAO;
+import ModelDTO.User;
+
+/**
+ * @author Pisal
+ *
+ */
+public class RemoveUserInfo implements Action {
+
+	/* (non-Javadoc)
+	 * @see Controller.Action#execute(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int id=Integer.parseInt(request.getParameter("user_id"));
+		/*String name = "Acer123";
+		String password = "121212";
+		String usertype = "admin";*/
+		
+		if(new UserDAO().delete(id)){
+			response.getWriter().write("User have been deleted.");
+			System.out.println("Yes");
+		}else{
+			response.getWriter().write("deleted Fail");
+		}
+		return null;
+	}
+
+}
