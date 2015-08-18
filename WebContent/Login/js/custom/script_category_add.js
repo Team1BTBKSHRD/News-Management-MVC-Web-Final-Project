9118fc25ba44bd978fb3cb445729662e59f65751
elit <chhornelit@gmail.com>
2015-08-18 10:36:49 +0700

    delete login file agian

--- WebContent/Login/js/custom/script_category_add.js
+++
@@ -1,4 +0,0 @@
-	
-		/*------------script for focus on menu -------------- */
-	    $("#main_menu_article").removeClass("parent" ).addClass("parent parent-focus" );
-		$("#sub_menu_managecategory").addClass("active" );
