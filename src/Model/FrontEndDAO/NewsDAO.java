d5cc131da0e1534a175146eb3e720ada27bf250b
elit <chhornelit@gmail.com>
2015-08-20 10:36:24 +0700

    merge with sambo/update dao

--- src/Model/FrontEndDAO/NewsDAO.java
+++ src/Model/FrontEndDAO/NewsDAO.java
@@ -16,10 +16,11 @@ public class NewsDAO {
 			e.printStackTrace();
 		}
 	}
-	public ResultSet listNewsCategoryByDaily(String categoryCode) throws SQLException, ClassNotFoundException {
+	public ResultSet listNewsCategoryByDaily(String categoryCode, int limit) throws SQLException, ClassNotFoundException {
 		try{
-			CallableStatement clstm = con.prepareCall("{call s_show_news_category_by_daily(?)}");
+			CallableStatement clstm = con.prepareCall("{call s_show_news_category_by_daily(?, ?)}");
 			clstm.setString(1, categoryCode);
+			clstm.setInt(2, limit);
 			ResultSet rs = clstm.executeQuery();
 			rs.next();
 			return rs;
@@ -32,10 +33,11 @@ public class NewsDAO {
 		return null;
 	}
 	
-	public ResultSet listNewsCategoryByWeekly(String categoryCode) throws SQLException {
+	public ResultSet listNewsCategoryByWeekly(String categoryCode, int limit) throws SQLException {
 		try{
-			CallableStatement clstm = con.prepareCall("{call s_show_news_category_by_weekly(?)}");
+			CallableStatement clstm = con.prepareCall("{call s_show_news_category_by_weekly(?, ?)}");
 			clstm.setString(1, categoryCode);
+			clstm.setInt(2, limit);
 			ResultSet rs = clstm.executeQuery();
 			rs.next();
 			return rs;
@@ -48,10 +50,11 @@ public class NewsDAO {
 		return null;
 	}
 	
-	public ResultSet listNewsCategoryByMonthly(String categoryCode) throws SQLException {
+	public ResultSet listNewsCategoryByMonthly(String categoryCode, int limit) throws SQLException {
 		try{
-			CallableStatement clstm = con.prepareCall("{call s_show_news_category_by_monthly(?)}");
+			CallableStatement clstm = con.prepareCall("{call s_show_news_category_by_monthly(?, ?)}");
 			clstm.setString(1, categoryCode);
+			clstm.setInt(2, limit);
 			ResultSet rs = clstm.executeQuery();
 			rs.next();
 			return rs;
@@ -64,12 +67,13 @@ public class NewsDAO {
 		return null;
 	}
 	
-	public ResultSet searchNewsFilterTime(String newsTitle, String categoryCode, String timeFilter) throws SQLException {
+	public ResultSet searchNewsFilterTime(String newsTitle, String categoryCode, String fullName, String timeFilter) throws SQLException {
 		try{
-			CallableStatement clstm = con.prepareCall("{call s_search_news_filter_time(?, ?, ?)}");
+			CallableStatement clstm = con.prepareCall("{call s_search_news_filter_time(?, ?, ?, ?)}");
 			clstm.setString(1, newsTitle);
 			clstm.setString(2, categoryCode);
-			clstm.setString(3, timeFilter);
+			clstm.setString(3, fullName);
+			clstm.setString(4, timeFilter);
 			System.out.println(clstm.toString());
 			ResultSet rs = clstm.executeQuery();
 			rs.next();
